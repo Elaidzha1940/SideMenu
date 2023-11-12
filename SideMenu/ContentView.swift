0c991408149a01dc22c19c112d00d2deb488f3a4
Elaidzha1940 <eli080293@gmail.com>
2023-11-12 14:49:51 +0300

    SideMenu

---
+++ SideMenu/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: SideMenu
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.11.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: SideMenu
+//  File: SideMenuApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.11.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
